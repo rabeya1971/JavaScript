438e07cf547546e183310f50520345944f38b34a
rabeya1971 <sultanarabeya21@gmail.com>
2026-02-22 00:47:44 +0600

    hhhh

---
+++ 01. Introduction/1.5 Connect JS file/main.js
@@ -0,0 +1,4 @@
+document.write("JavaScript");
+        console.log("JavaScript is a programming language");
+        alert("JavaScript");
+        document.getElementById("output").innerHTML = "Client Side Language." ;
