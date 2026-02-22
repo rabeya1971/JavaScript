b74eb424efd2cd2fb1e84eee3746199901ca88e4
rabeya1971 <sultanarabeya21@gmail.com>
2026-02-22 23:48:56 +0600

    added

---
+++ 01. Introduction/1.6 Variable/variable.js
@@ -0,0 +1,26 @@
+/*
+var x;
+x = 10;
+document.write(x);
+console.log(x);
+JavaScript variables are containers for data.
+a-z, A-Z, name, Name, _name, $name, 
+case sensitive
+camel case practice: myName, productName, bestProductPrice
+reserve keyword dont use as a variable name:let, var,for, while, const
+ 3 types: var,let,const*/
+
+ var carName = "volvo";
+ var carName = "toyota";
+ console.log(carName);
+
+    let x = 5;
+    let y = 6;
+    let z = x + y;
+    document.getElementById("demo").innerHTML = "The value of z is " +z ;
+
+    let Name = "Rabeya";
+    Name = "Tashfeen";
+    console.log(Name);
+    document.write(Name);
+    document.getElementById("me").innerHTML = "My Name is "+Name 
